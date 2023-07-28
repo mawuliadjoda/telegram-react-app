59db4926946ab3cecf55eaa963f5633e81de2212
Mawuli ADJODA <koffimawulli.adjoda@gmail.com>
2023-07-28 23:54:21 +0000

    fix deploy

--- src/db/db.js
+++ src/db/db.js
@@ -8,12 +8,12 @@ import kebabImg from "../images/kebab.png";
 
 export function getData() {
   return [
-    { title: "Pizza", price: 17.99, Image: pizzaImg,id:1 },
-    { title: "Burger", price: 15, Image: burgerImg,id:2 },
-    { title: "Coca", price: 3.5, Image: cocaImg ,id:3},
-    { title: "Kebab", price: 13.99, Image: kebabImg,id:4 },
-    { title: "Salad", price: 2.5, Image: saladImg,id:5 },
-    { title: "Bottle of water", price: 0.99, Image: waterImg,id:6 },
-    { title: "Ice cream", price: 2.99, Image: iceCreamImg,id:7 },
+    { title: "Pizza", price: 17.99, Image: pizzaImg },
+    { title: "Burger", price: 15, Image: burgerImg },
+    { title: "Coca", price: 3.5, Image: cocaImg },
+    { title: "Kebab", price: 13.99, Image: kebabImg },
+    { title: "Salad", price: 2.5, Image: saladImg},
+    { title: "Bottle of water", price: 0.99, Image: waterImg},
+    { title: "Ice cream", price: 2.99, Image: iceCreamImg},
   ];
 }
